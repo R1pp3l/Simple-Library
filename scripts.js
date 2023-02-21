275940b993eb66d21c167ee9e109eaa6e19ec4f0
Victor Cabral <victorcabralweb@outlook.com>
2023-02-21 22:04:31 +0800

    Added delete function

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
 const myLibrary = [];
-let counter = 0;
+const counter = 0;
 const bookContainer = document.getElementById("book-container");
+const bookCount = document.querySelector(".book-count .value");
 
 function Book(title, author, pages, status) {
+  this.id = counter;
   this.title = title;
   this.author = author;
   this.pages = pages;
@@ -12,8 +14,7 @@ function Book(title, author, pages, status) {
 function renderBook(book) {
   const cardCell = document.createElement("div");
   cardCell.classList.add("cards-cell");
-  cardCell.id = `cards-cell-${counter}`;
-  counter += 1;
+  cardCell.id = `cards-cell-${book.id}`;
 
   const card = document.createElement("div");
   card.classList.add("cards");
@@ -42,14 +43,59 @@ function renderBook(book) {
   cardList.appendChild(pages);
   cardList.appendChild(status);
 
+  const deleteButton = document.createElement("button");
+  deleteButton.classList.add("card-buttons");
+  deleteButton.textContent = "Delete";
+  deleteButton.addEventListener("click", () => {
+    // eslint-disable-next-line no-use-before-define
+    deleteBook(book.id);
+    bookContainer.removeChild(cardCell);
+    // eslint-disable-next-line no-use-before-define
+    updateBookCount();
+  });
+
+  const readButton = document.createElement("button");
+  readButton.classList.add("card-buttons");
+  readButton.textContent = "Change";
+  readButton.addEventListener("click", () => {
+    if (book.status === "Not Read") {
+      // eslint-disable-next-line no-param-reassign
+      book.status = "Read";
+    } else {
+      // eslint-disable-next-line no-param-reassign
+      book.status = "Not Read";
+    }
+    status.textContent = book.status;
+  });
+
+  const buttonList = document.createElement("ul");
+  buttonList.classList.add("card-buttons-list");
+  const deleteButtonLi = document.createElement("li");
+  const readButtonLi = document.createElement("li");
+  deleteButtonLi.appendChild(deleteButton);
+  readButtonLi.appendChild(readButton);
+  buttonList.appendChild(deleteButtonLi);
+  buttonList.appendChild(readButtonLi);
+
   card.appendChild(cardList);
+  card.appendChild(buttonList);
   cardCell.appendChild(card);
   bookContainer.appendChild(cardCell);
+}
+
+function updateBookCount() {
+  bookCount.textContent = myLibrary.length;
 }
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
   renderBook(book);
+  updateBookCount();
+}
+
+function deleteBook(bookId) {
+  const bookIndex = myLibrary.findIndex((book) => book.id === bookId);
+  myLibrary.splice(bookIndex, 1);
 }
 
 const form = document.querySelector("form");
@@ -68,21 +114,33 @@ function showModal() {
   modal.style.display = "flex";
 }
 
-function hideModal() {
-  modal.style.display = "none";
-}
-
 addBookButton.addEventListener("click", () => {
   showModal();
 });
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
+function hideModal() {
+  modal.style.display = "none";
+}
+
+function validateForm(title, author, pages) {
+  if (title === "" || author === "" || pages === "") {
+    alert("Please fill in all fields.");
+    return false;
+  }
+  return true;
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
   const title = titleInput.value;
   const author = authorInput.value;
-  const page = pageInput.value;
+  const pages = pageInput.value;
   const status = statusSelect.value;
-  const book = new Book(title, author, page, status);
-  addBookToLibrary(book);
-  hideModal();
+
+  if (validateForm(title, author, pages)) {
+    const book = new Book(title, author, pages, status);
+    addBookToLibrary(book);
+    hideModal();
+    form.reset();
+  }
 });
